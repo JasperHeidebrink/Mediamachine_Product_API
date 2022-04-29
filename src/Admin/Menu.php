59324f8f19f53fcfa297d91f4a9c9d773ece91af
Jasper Heidebrink <jasper@dragonet.nl>
2022-04-29 13:21:11 +0200

    clean admin

--- src/Admin/Menu.php
+++ src/Admin/Menu.php
@@ -17,23 +17,7 @@ class Menu {
 			return;
 		}
 
-		if ( ! is_plugin_active( "{$this->parent_plugin_name}/{$this->parent_plugin_name}.php" ) ) {
-			add_action( 'admin_notices', [ $this, 'requirements_notices' ] );
-		}
-
 		add_action( 'admin_menu', [ $this, 'add_event_page' ] );
-	}
-
-	/**
-	 * @return void
-	 */
-	public function requirements_notices(): void {
-		echo '<div class="notice notice-warning is-dismissible"><p>';
-		esc_html_e(
-			'DPG Event API plugin is currently not working: plugin sm-main is needed. Please install or activate the plugin.',
-			DPG_EVENTAPI_SLUG
-		);
-		echo '</p></div>';
 	}
 
 	/**
@@ -65,48 +49,5 @@ class Menu {
 		$context['title'] = 'DPG Event API';
 
 		Timber::render( 'admin/options.twig', $context );
-	}
-
-	/**
-	 * Get the settings option array and print one of its values
-	 */
-	public function dropdown_events(): void {
-		$events  = [
-			'id'    => 0,
-			'title' => 'example event',
-		];
-		$options = get_option( 'eventapi_options_group' );
-
-		echo '<select id="event_id" name="eventapi_options_group[event_id]">';
-		echo '<option value="">' . __( 'Please select a event', DPG_EVENTAPI_SLUG ) . '</option>';
-
-		foreach ( $events as $event ) {
-			$selected = ( $options['event_id'] === $event['id'] ) ? 'selected="selected"' : '';
-			echo '<option value="' . $event['id'] . '" ' . $selected . '>' . $event['title'] . '</option>';
-		}
-		echo '</select>';
-	}
-
-	/**
-	 * Get the settings option array and print one of its values
-	 */
-	public function dropdown_editions(): void {
-		$options = get_option( 'eventapi_options_group' );
-
-		if ( empty( $options['event_id'] ) ) {
-			return __( 'First you have to select a event', DPG_EVENTAPI_SLUG );
-		}
-
-		$editions = [
-			'id'    => 0,
-			'title' => 'example event',
-		];
-		echo '<select id="edition_id" name="eventapi_options_group[edition_id]">';
-		echo '<option value="">' . __( 'Please select a edition', DPG_EVENTAPI_SLUG ) . '</option>';
-		foreach ( $editions as $key => $edition ) {
-			$selected = ( $options['edition_id'] === $edition['id'] ) ? 'selected="selected"' : '';
-			echo '<option value="' . $edition['id'] . '" ' . $selected . '>' . $edition['title'] . '</option>';
-		}
-		echo '</select>';
 	}
 }
