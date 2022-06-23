312db9e7ef4d00cd9b65b8d244a72a1886abcac7
Augus van Gils <augus.van.gils@persgroep.net>
2022-06-23 16:14:15 +0200

    Better event filter

--- assets/default.js
+++ assets/default.js
@@ -2,54 +2,62 @@
 /**
  * @package   DPG_WP_EventApi
  */
-const EVENTAPI_SHOW_SPEED = 100;
+const EVENTAPI_CHANGE_SPEED = 100;
 
 jQuery(document).ready(function ($) {
 
-    $('select[name^="event__selectfilter"]').change(function () {
-        DpgEventApi_apply_filter();
+    let text_search = $('input[name="event_filter_text"]'),
+        category_search = $('select[name="event_filter_activity"]'),
+        date_search = $('select[name="event_filter_date"]'),
+        empty_container = $('.c-event-no-result');
+
+    empty_container.hide();
+
+    text_search.on('keyup', function () {
+        dpg_event_filter()
     });
-    $('input[name="search_participants_query"]').keyup(function () {
-        DpgEventApi_apply_filter();
+    category_search.on('change', function () {
+        dpg_event_filter()
+    });
+    date_search.on('change', function () {
+        dpg_event_filter()
     });
 
-    function DpgEventApi_apply_filter() {
-        let category = $('select[name="event__selectfilter_category"]').val(),
-            day = $('select[name="event__selectfilter_day"]').val(),
-            text = $('input[name="search_participants_query"]').val(),
-            empty_result = $('.event_empty_result');
-
-        empty_result.hide();
+    function dpg_event_filter() {
+        let text_value = text_search.val(),
+            category_value = category_search.val(),
+            date_value = date_search.val(),
+            block_visible = false;
 
         $('.event__block').each(function () {
-            let item = $(this),
-                item_text = $('.c-card', item).text();
+            let blockText = $(this).text().toLowerCase(),
+                item_shown = true;
 
-            if ('0' === day && '0' === category) {
-                item.show(EVENTAPI_SHOW_SPEED);
-            } else if ('0' !== day && '0' !== category &&
-                category === item.attr('data-category') && day === item.attr('data-day')) {
-                item.show(EVENTAPI_SHOW_SPEED);
-            } else if ('0' === day && category === item.attr('data-category')) {
-                item.show(EVENTAPI_SHOW_SPEED);
-            } else if ('0' === category && day === item.attr('data-day')) {
-                item.show(EVENTAPI_SHOW_SPEED);
+
+            if (text_value && ! blockText.includes(text_value.toLowerCase())) {
+                item_shown = false;
+            }
+
+            if ( item_shown && '0' !== category_value && category_value !== $(this).attr('data-category')) {
+                item_shown = false;
+            }
+
+            if ( item_shown && '0' !== date_value && date_value !== $(this).attr('data-day')) {
+                item_shown = false;
+            }
+
+            if (item_shown) {
+                block_visible = true;
+                $(this).show(EVENTAPI_CHANGE_SPEED);
             } else {
-                item.hide();
-                return;
-            }
-
-            if ('' === text) {
-                return;
-            }
-
-            if (item_text.toLowerCase().search(text.toLowerCase()) === -1) {
-                item.hide();
+                $(this).hide(EVENTAPI_CHANGE_SPEED);
             }
         });
 
-        if ($('.event__block:visible').length === 0) {
-            empty_result.show(EVENTAPI_SHOW_SPEED * 2);
+        if ( block_visible ) {
+            empty_container.hide();
+        } else {
+            empty_container.show();
         }
     }
 });
