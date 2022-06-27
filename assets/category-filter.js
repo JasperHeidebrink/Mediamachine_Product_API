5fca3630fd3a1f012214f6349ebc106414fa80b4
Augus van Gils <augus.van.gils@persgroep.net>
2022-06-27 14:36:55 +0200

    Add prevent default

--- assets/category-filter.js
+++ assets/category-filter.js
@@ -7,19 +7,22 @@ jQuery(document).ready(function ($) {
 
     showCategories();
 
-    $(".c-event-activities-category nav a").on('click', function (){
+    $(".c-event-activities-category nav a").on('click', function (e){
+        e.preventDefault();
         $('.c-event-activities-category nav a').removeClass('active');
         $(this).addClass('active');
 
         showCategories();
     });
 
-    $(".c-event-category .day .title").on('click', function (){
+    $(".c-event-category .day .title").on('click', function (e){
+        e.preventDefault();
         $(this).siblings().toggle();
         $(this).toggleClass('open');
     });
 
-    $(".c-event-category .day .activities .activity a").on('click', function (){
+    $(".c-event-category .day .activities .activity a").on('click', function (e){
+        e.preventDefault();
         $(this).siblings().toggleClass('open');
     });
 
