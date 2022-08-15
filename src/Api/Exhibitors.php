041c8270593178c536b374ecf0132b8b1b2d5a31
Augus van Gils <augus.van.gils@persgroep.net>
2022-08-15 11:34:57 +0200

    First changes

--- src/Api/Exhibitors.php
+++ src/Api/Exhibitors.php
@@ -4,43 +4,113 @@ namespace DPG\WordPress\EventApi\Api;
 
 use DPG\WordPress\EventApi\Models\Exhibitor;
 
-class Exhibitors
-{
-    /**
-     * @return array
-     */
-    public static function getAll(): array
-    {
-        $current_edition_id = (int)get_option('eventapi_edition_id');
-        $response           = EventApi::get('exhibitor', ['editionId' => $current_edition_id]);
+class Exhibitors {
+	/**
+	 * @return array
+	 */
+	public static function getAll(): array {
+		$current_edition_id = (int) get_option( 'eventapi_edition_id' );
+		$response           = EventApi::get( 'exhibitor', [ 'editionId' => $current_edition_id ] );
 
-        if (empty($response)) {
-            return [
-                self::fillItem(['title' => 'No exhibitors available']),
-            ];
-        }
+		if ( empty( $response ) ) {
+			return [
+				self::fillItem( [ 'title' => 'No exhibitors available' ] ),
+			];
+		}
 
-        $events = [];
-        foreach ($response['items'] as $item) {
-            $events[] = self::fillItem($item);
-        }
+		$events = [];
+		foreach ( $response['items'] as $item ) {
+			$events[] = self::fillItem( $item );
+		}
 
-        return $events;
-    }
+		return $events;
+	}
 
-    /**
-     * @param array $data
-     *
-     * @return Exhibitor
-     */
-    public static function fillItem(array $data): Exhibitor
-    {
-        return new Exhibitor(
-            $data['id'] ?? 0,
-            $data['title'] ?? '',
-            $data['url'] ?? '',
-            $data['stand'] ?? '',
-            $data['branches'] ?? '',
-        );
-    }
+	/**
+	 * @param array $data
+	 *
+	 * @return Exhibitor
+	 */
+	public static function fillItem( array $data ): Exhibitor {
+		return new Exhibitor(
+			$data['id'] ?? 0,
+			$data['title'] ?? '',
+			$data['url'] ?? '',
+			$data['stand'] ?? '',
+			$data['branches'] ?? '',
+		);
+	}
+
+	/**
+	 * @param string $search_shop_query
+	 *
+	 * @return array
+	 */
+	public function getShops( string $search_query = '', $divide_in_categories = true ): array {
+		$data               = [];
+		$data['categories'] = [];
+		$exhibitors         = self::getAll();
+
+		/**
+		 * @var $exhibitor Exhibitor
+		 */
+		foreach ( $exhibitors as $exhibitor ) {
+			if ( ! self::need_to_add_exhibitor( $exhibitor->getStand(), $search_query ) ) {
+				continue;
+			}
+
+			if ( $divide_in_categories ) {
+				unset( $data['categories'] );
+
+				foreach ( $exhibitor->getBranches() as $branch ) {
+
+					if ( ! isset( $data[ $branch ] ) ) {
+						$data[ $branch ] = [ 'shops' => [] ];
+					}
+
+					$data[ $branch ]['shops'][] = $exhibitor;
+
+					if ( empty( $data[ $branch ]['shops'] ) ) {
+						unset( $data[ $branch ] );
+					}
+				}
+			} else {
+				foreach ( $exhibitor->getBranches() as $branch ) {
+					if ( ! in_array( $branch, $data['categories'] ) ) {
+						$data['categories'][] = $branch;
+					}
+				}
+				$data['exhibitors'][] = $exhibitor;
+			}
+		}
+
+		if ( $divide_in_categories ) {
+			ksort( $data );
+		} else {
+			sort( $data['categories'] );
+			shuffle( $data['exhibitors'] );
+		}
+
+		return $data;
+	}
+
+	/**
+	 * @param array $stand
+	 * @param string $search_shop_query
+	 *
+	 * @return bool
+	 */
+	private static function need_to_add_exhibitor( array $stand, string $search_shop_query = '' ): bool {
+		if ( empty( $search_shop_query ) ) {
+			return true;
+		}
+
+		if ( str_contains( strtolower( $stand['name'] ), strtolower( $search_shop_query ) ) ) {
+			return true;
+		}
+
+		return false;
+	}
+
+
 }
--- src/Shortcode.php
+++ src/Shortcode.php
@@ -1,22 +1,101 @@
 <?php
-/**
- * Usage:
- * [dpg-ep-activities]
- * [dpg-ep-shops]
- * [dpg-ep-program]
- */
 
 namespace DPG\WordPress\EventApi;
 
-use DPG\WordPress\EventApi\Shortcodes\Activity;
-use DPG\WordPress\EventApi\Shortcodes\Program;
-use DPG\WordPress\EventApi\Shortcodes\Shops;
+use DPG\WordPress\EventApi\Api\Activities;
+use DPG\WordPress\EventApi\Api\Exhibitors;
+use Timber\Timber;
 
 class Shortcode {
+
 	public function __construct() {
-		add_shortcode( 'dpg-ep-activities', [ new Activity, 'get_html' ] );
-		add_shortcode( 'dpg-ep-shops', [ new Shops, 'get_html' ] );
-		add_shortcode( 'dpg-ep-program-category', [ new Program, 'get_category_html' ] );
-		add_shortcode( 'dpg-ep-program', [ new Program, 'get_html' ] );
+		add_shortcode( 'dpg-ep-activities', [ $this, 'get_activities_content' ] );
+		add_shortcode( 'dpg-ep-shops', [ $this, 'get_shops_content' ] );
+		add_shortcode( 'dpg-ep-shops-extended', [ $this, 'get_shops_extended_content' ] );
+		add_shortcode( 'dpg-ep-program-category', [ $this, 'get_program_categorized_content' ] );
+		add_shortcode( 'dpg-ep-program', [ $this, 'get_program_content' ] );
+	}
+
+	/**
+	 * Used for generating (shortcode) content with Timber.
+	 *
+	 * @return string activity list as html/string or empty on failure.
+	 */
+	public function get_activities_content(): string {
+		$context                 = Timber::context();
+		$context['activityList'] = Activities::getAll();
+
+		return Timber::compile( 'event-api-frontend/activities.twig', $context ) ?: '';
+	}
+
+	/**
+	 * Used for generating (shortcode) content with Timber.
+	 *
+	 * @return string program list as html/string or empty on failure.
+	 */
+	public function get_program_content(): string {
+		$context                           = Timber::context();
+		$context['activityListByTimeslot'] = Activities::getGroupedByTimeslot();
+		$context['activityList']           = Activities::getAll( true, true );
+		$context['dayList']                = Activities::getDaysList();
+		$context['categoryList']           = Activities::getCategoryList();
+		$context['active_activity']        = key( $context['activityListByTimeslot'] );
+		$context['default_image']          = DPG_EVENTAPI_URL . '/assets/placeholder.png';
+
+		wp_enqueue_script( 'dpg-event-filter' );
+
+		return Timber::compile( 'event-api-frontend/program.twig', $context ) ?: '';
+	}
+
+	/**
+	 * Used for generating (shortcode) content with Timber.
+	 *
+	 * @return string categorized program list as html/string or empty on failure.
+	 */
+	public function get_program_categorized_content(): string {
+		$context                           = Timber::context();
+		$context['activityListByTimeslot'] = Activities::getGroupedByTimeslot();
+		$context['active_activity']        = key( $context['activityListByTimeslot'] );
+		$context['default_image']          = DPG_EVENTAPI_URL . '/assets/placeholder.png';
+
+		wp_enqueue_script( 'dpg-event-category-filter' );
+
+		return Timber::compile( 'event-api-frontend/program_by_category.twig', $context ) ?: '';
+	}
+
+	/**
+	 * Used for generating (shortcode) content with Timber.
+	 *
+	 * @TODO: remove shop seaarch and handle with jquery
+	 *
+	 * @return string shop list as html/string or empty on failure.
+	 */
+	public function get_shops_content(): string {
+
+		$context                      = Timber::context();
+		$context['search_shop_query'] = $_GET['shop'] ? filter_var( $_GET['shop'], FILTER_SANITIZE_STRING ) : '';
+		$context['shops']             = ( new Exhibitors )->getShops( $context['search_shop_query'] );
+
+		wp_enqueue_script( 'dpg-event-shops' );
+
+		return Timber::compile( 'event-api-frontend/shops.twig', $context ) ?: '';
+	}
+
+	/**
+	 * Used for generating (shortcode) content with Timber.
+	 *
+	 * @return string shop list as html/string or empty on failure.
+	 */
+	public function get_shops_extended_content() {
+
+		$context                      = Timber::context();
+		$shops                        = ( new Exhibitors )->getShops(false );
+		$context['shops']             = $shops['exhibitors'];
+		$context['categories']        = $shops['categories'];
+		$context['default_image']     = DPG_EVENTAPI_URL . '/assets/placeholder.png';
+
+		wp_enqueue_script( 'dpg-event-shops' );
+
+		return Timber::compile( 'event-api-frontend/shops-extended.twig', $context ) ?: '';
 	}
 }
