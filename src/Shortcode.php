9207005778014ea6bef081d25a62b6c4f20ce70c
Augus van Gils <augus.van.gils@persgroep.net>
2022-08-18 12:07:40 +0200

    Add settings for shortcodes

--- src/Shortcode.php
+++ src/Shortcode.php
@@ -23,6 +23,7 @@ class Shortcode {
 	 * @return string activity list as html/string or empty on failure.
 	 */
 	public function get_activities_content(): string {
+
 		$context                 = Timber::context();
 		$context['activityList'] = Activities::getAll();
 
@@ -32,51 +33,21 @@ class Shortcode {
 	/**
 	 * Used for generating (shortcode) content with Timber.
 	 *
-	 * @return string program list as html/string or empty on failure.
-	 */
-	public function get_program_content(): string {
-		$context                           = Timber::context();
-		$context['activityListByTimeslot'] = Activities::getGroupedByTimeslot();
-		$context['activityList']           = Activities::getAll( true, true );
-		$context['dayList']                = Activities::getDaysList();
-		$context['categoryList']           = Activities::getCategoryList();
-		$context['active_activity']        = key( $context['activityListByTimeslot'] );
-		$context['default_image']          = DPG_EVENTAPI_URL . '/assets/placeholder.png';
-
-		wp_enqueue_script( 'dpg-event-filter' );
-
-		return Timber::compile( 'event-api-frontend/program.twig', $context ) ?: '';
-	}
-
-	/**
-	 * Used for generating (shortcode) content with Timber.
-	 *
-	 * @return string categorized program list as html/string or empty on failure.
-	 */
-	public function get_program_categorized_content(): string {
-		$context                           = Timber::context();
-		$context['activityListByTimeslot'] = Activities::getGroupedByTimeslot();
-		$context['active_activity']        = key( $context['activityListByTimeslot'] );
-		$context['default_image']          = DPG_EVENTAPI_URL . '/assets/placeholder.png';
-
-		wp_enqueue_script( 'dpg-event-category-filter' );
-
-		return Timber::compile( 'event-api-frontend/program_by_category.twig', $context ) ?: '';
-	}
-
-	/**
-	 * Used for generating (shortcode) content with Timber.
-	 *
-	 * @TODO: remove shop seaarch and handle with jquery
-	 *
 	 * @return string shop list as html/string or empty on failure.
 	 */
-	public function get_shops_content(): string {
+	public function get_shops_content( $atts = [] ): string {
+
+		$settings = shortcode_atts(
+			[
+				'link_target' => '_blank',
+			], $atts
+		);
 
 		$context                  = Timber::context();
 		$shops                    = new Shops();
 		$context['branches']      = $shops->get_shops_categorised();
-		$context['default_image'] = get_option( 'event_api_default_image') ?? DPG_EVENTAPI_URL . '/assets/placeholder.png';
+		$context['default_image'] = get_option( 'event_api_default_image' ) ?? DPG_EVENTAPI_URL . '/assets/placeholder.png';
+		$context['settings']      = $settings;
 
 		wp_enqueue_script( 'dpg-event-shops' );
 
@@ -88,16 +59,85 @@ class Shortcode {
 	 *
 	 * @return string shop list as html/string or empty on failure.
 	 */
-	public function get_shops_extended_content() {
+	public function get_shops_extended_content( $atts = [] ) {
+
+		$settings = shortcode_atts(
+			[
+				'link_target' => '_blank',
+				'button_text' => 'Website'
+			], $atts
+		);
 
 		$context                  = Timber::context();
 		$shops                    = new Shops();
 		$context['shops']         = $shops->get_shops_at_random();
 		$context['categories']    = $shops->get_categories();
-		$context['default_image'] = get_option( 'event_api_default_image') ?? DPG_EVENTAPI_URL . '/assets/placeholder.png';
+		$context['default_image'] = get_option( 'event_api_default_image' ) ?? DPG_EVENTAPI_URL . '/assets/placeholder.png';
+		$context['settings']      = $settings;
 
 		wp_enqueue_script( 'dpg-event-shops' );
 
 		return Timber::compile( 'event-api-frontend/shops-extended.twig', $context ) ?: '';
+	}
+
+	/**
+	 * Used for generating (shortcode) content with Timber.
+	 *
+	 * @return string categorized program list as html/string or empty on failure.
+	 */
+	public function get_program_categorized_content( $atts = [] ): string {
+
+		$settings = shortcode_atts(
+			[
+				'link_target'   => '_blank',
+				'button_text'   => 'Lees meer',
+				'button_link'   => 'website',
+				'hide_location' => 'false',
+				'show_price'    => 'false',
+				'free_text'     => 'gratis'
+			], $atts
+		);
+
+		$context                           = Timber::context();
+		$context['activityListByTimeslot'] = Activities::getGroupedByTimeslot();
+		$context['active_activity']        = key( $context['activityListByTimeslot'] );
+		$context['default_image']          = get_option( 'event_api_default_image' ) ?? DPG_EVENTAPI_URL . '/assets/placeholder.png';
+		$context['settings']               = $settings;
+
+		wp_enqueue_script( 'dpg-event-category-filter' );
+
+		return Timber::compile( 'event-api-frontend/program_by_category.twig', $context ) ?: '';
+	}
+
+	/**
+	 * Used for generating (shortcode) content with Timber.
+	 *
+	 * @return string program list as html/string or empty on failure.
+	 */
+	public function get_program_content( $atts = [] ): string {
+
+		$settings = shortcode_atts(
+			array(
+				'link_target'   => '_blank',
+				'button_text'   => 'Boek je ticket nu',
+				'button_link'   => 'website',
+				'hide_location' => 'false',
+				'show_price'    => 'false',
+				'free_text'     => 'gratis'
+			), $atts
+		);
+
+		$context                           = Timber::context();
+		$context['activityListByTimeslot'] = Activities::getGroupedByTimeslot();
+		$context['activityList']           = Activities::getAll( true, true );
+		$context['dayList']                = Activities::getDaysList();
+		$context['categoryList']           = Activities::getCategoryList();
+		$context['active_activity']        = key( $context['activityListByTimeslot'] );
+		$context['default_image']          = get_option( 'event_api_default_image' ) ?? DPG_EVENTAPI_URL . '/assets/placeholder.png';
+		$context['settings']               = $settings;
+
+		wp_enqueue_script( 'dpg-event-filter' );
+
+		return Timber::compile( 'event-api-frontend/program.twig', $context ) ?: '';
 	}
 }
